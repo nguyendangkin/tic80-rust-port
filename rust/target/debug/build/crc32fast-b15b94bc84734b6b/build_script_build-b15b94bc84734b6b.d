cf20bd8e657a11b80139caae352a7d45fac9932c
nguyendangkin <nguyenchin0077@gmail.com>
2026-06-18 18:07:53 +0700

    Remove rust/target/ from git tracking + update .gitignore

--- rust/target/debug/build/crc32fast-b15b94bc84734b6b/build_script_build-b15b94bc84734b6b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\nguye\Downloads\TIC-80-main\TIC-80-main\rust\target\debug\build\crc32fast-b15b94bc84734b6b\build_script_build-b15b94bc84734b6b.d: C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs
-
-C:\Users\nguye\Downloads\TIC-80-main\TIC-80-main\rust\target\debug\build\crc32fast-b15b94bc84734b6b\build_script_build-b15b94bc84734b6b.exe: C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs
-
-C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs:
--- rust/target/debug/deps/adler2-26f2c26b2dca01c7.d
+++
@@ -1,8 +0,0 @@
-C:\Users\nguye\Downloads\TIC-80-main\TIC-80-main\rust\target\debug\deps\adler2-26f2c26b2dca01c7.d: C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\lib.rs C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\algo.rs
-
-C:\Users\nguye\Downloads\TIC-80-main\TIC-80-main\rust\target\debug\deps\libadler2-26f2c26b2dca01c7.rlib: C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\lib.rs C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\algo.rs
-
-C:\Users\nguye\Downloads\TIC-80-main\TIC-80-main\rust\target\debug\deps\libadler2-26f2c26b2dca01c7.rmeta: C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\lib.rs C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\algo.rs
-
-C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\lib.rs:
-C:\Users\nguye\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\algo.rs:
